6eee30f8d8bc4f6b3874b9caecb59a8266268d20
jeanmajid <jeanmajid@users.noreply.github.com>
2024-01-28 23:21:54 +0100

    test2

---
+++ build.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const archiver = require("archiver");
+
+const output = fs.createWriteStream(__dirname + "/your-pack-name.mcpack");
+const archive = archiver("zip", {
+    zlib: { level: 9 },
+});
+
+archive.pipe(output);
+
+archive.glob("**", {
+    ignore: ["node_modules/**", ".git/**", ".github/**", "*.mcpack", "package-lock.json", "package.json"],
+});
+
+archive.finalize();
