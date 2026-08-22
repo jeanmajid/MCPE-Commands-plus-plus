e3df9294f38e6a1d552dc9ec27200643d2d27e20
jeanmajid <jeanhodul@gmail.com>
2026-08-22 23:52:57 +0200

    chore: rename target to targets and target and whatever
    Co-authored-by: MaxedOut4826 <MaxedOut4826@users.noreply.github.com>

--- BP/scripts/commands/registry/entity/top.ts
+++ BP/scripts/commands/registry/entity/top.ts
@@ -43,25 +43,26 @@ CommandManager.registerCommand(
             { name: "minHeight", type: CustomCommandParamType.Float },
         ],
     },
-    (origin, target: Entity[], minHeight?: number) => {
+    (origin, targets: Entity[], minHeight?: number) => {
         system.run(() => {
-            if (!target && origin.sourceEntity) {
+            if (!targets && origin.sourceEntity) {
                 teleportEntityToTop(origin.sourceEntity, minHeight);
                 return;
             }
-            for (const entity of target) {
-                teleportEntityToTop(entity, minHeight);
+
+            for (const target of targets) {
+                teleportEntityToTop(target, minHeight);
             }
         });
         return { status: CustomCommandStatus.Success, message: "Sucessfully teleported entities" };
     }
 );
 
-function teleportEntityToTop(entity: Entity, minHeight?: number): void {
-    const { x, y, z } = entity.location;
-    const topBlock = entity.dimension.getTopmostBlock({ x, z }, minHeight);
+function teleportEntityToTop(target: Entity, minHeight?: number): void {
+    const { x, y, z } = target.location;
+    const topBlock = target.dimension.getTopmostBlock({ x, z }, minHeight);
     if (!topBlock || topBlock.y <= y) {
         return;
     }
-    entity.teleport({ x, y: topBlock.y + 1, z });
+    target.teleport({ x, y: topBlock.y + 1, z });
 }
