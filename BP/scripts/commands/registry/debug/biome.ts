f2ef839a86feb1cd20c604bbc2f980a2a162a88c
MaxedOut4826 <maximreed@hotmail.com>
2026-09-12 15:35:39 +0100

    feat: merge biome & biomeTags debug commands into biome command with enum

--- BP/scripts/commands/registry/debug/biome.ts
+++ BP/scripts/commands/registry/debug/biome.ts
@@ -21,23 +21,60 @@
  * along with Commands Plus Plus. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { CommandPermissionLevel, CustomCommandStatus } from "@minecraft/server";
+import {
+    BiomeType,
+    CommandPermissionLevel,
+    CustomCommandParamType,
+    CustomCommandStatus,
+} from "@minecraft/server";
 
 import { CommandManager } from "../../command.js";
+
+const BIOME_DATA_ENUM_KEY = "biomeDataEnum";
+enum BiomeData {
+    all = "all",
+    tags = "tags",
+    id = "id",
+}
+
+CommandManager.registerEnum(BIOME_DATA_ENUM_KEY, Object.values(BiomeData));
 
 CommandManager.registerCommand(
     {
         name: "biome",
-        description: "Outputs the identifier for the biome based on the position of the executor",
+        description: "Outputs the data for the biome based on the position of the executor",
         permissionLevel: CommandPermissionLevel.GameDirectors,
+        optionalParameters: [
+            { name: "biomeData", type: CustomCommandParamType.Enum, enumName: BIOME_DATA_ENUM_KEY },
+        ],
     },
-    (origin) => {
+    (origin, biomeData: BiomeData = BiomeData.id) => {
         const source = origin.sourceEntity ?? origin.sourceBlock ?? origin.initiator;
         if (!source || !source.dimension.isChunkLoaded(source.location)) {
-            return { status: CustomCommandStatus.Failure };
+            return {
+                status: CustomCommandStatus.Failure,
+                message: "Cannot get biome data outside of the world",
+            };
         }
 
         const biome = source.dimension.getBiome(source.location);
-        return { status: CustomCommandStatus.Success, message: `§aBiome: §r${biome.id}` };
+        switch (biomeData) {
+            case BiomeData.all:
+                return {
+                    status: CustomCommandStatus.Success,
+                    message: `§aBiome: §r${biome.id}\n§aTags: §r${getBiomeTags(biome)}`,
+                };
+            case BiomeData.id:
+                return { status: CustomCommandStatus.Success, message: `§aBiome: §r${biome.id}` };
+            case BiomeData.tags:
+                return {
+                    status: CustomCommandStatus.Success,
+                    message: `§aTags: §r${getBiomeTags(biome)}`,
+                };
+        }
     }
 );
+
+function getBiomeTags(biome: BiomeType): string {
+    return biome.getTags().join(", ");
+}
