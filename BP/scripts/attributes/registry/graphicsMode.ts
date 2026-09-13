92f8920114d7f843f042d8a38886d9ff4db959c9
MaxedOut4826 <maximreed@hotmail.com>
2026-09-13 22:07:25 +0100

    feat: add graphicsMode attribute

---
+++ BP/scripts/attributes/registry/graphicsMode.ts
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later
+ * ============================================================================
+ * Commands Plus Plus
+ * Copyright (C) 2024-2026 jeanmajid and contributors
+ * https://github.com/jeanmajid/MCPE-Commands-plus-plus
+ * ============================================================================
+ *
+ * This file is part of Commands Plus Plus.
+ *
+ * Commands Plus Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Commands Plus Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Commands Plus Plus. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { GraphicsMode, ScoreboardObjective, system, world } from "@minecraft/server";
+
+import { AttributeManager, BaseAttribute } from "../attribute";
+
+const graphicsModeIndex = {
+    [GraphicsMode.Simple]: 0,
+    [GraphicsMode.Fancy]: 1,
+    [GraphicsMode.Deferred]: 2,
+    [GraphicsMode.RayTraced]: 3,
+};
+
+class GraphicsModeAttribute extends BaseAttribute {
+    public id = "graphicsmode";
+    public runId = -1;
+
+    public initialize(): void {
+        this.runId = system.runInterval(() => {
+            this.setValues(this.score);
+        }, 1);
+    }
+
+    public setValues(score: ScoreboardObjective): void {
+        for (const player of world.getAllPlayers()) {
+            score.setScore(player, graphicsModeIndex[player.graphicsMode]);
+        }
+    }
+
+    public cleanup(): void {
+        system.clearRun(this.runId);
+    }
+}
+
+AttributeManager.registerAttribute(new GraphicsModeAttribute());
