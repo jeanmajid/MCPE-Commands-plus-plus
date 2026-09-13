972a6a7d1a209b4d62261ff5a05d1b97eaaba7b9
MaxedOut4826 <maximreed@hotmail.com>
2026-09-13 22:08:31 +0100

    feat: add scale attribute

---
+++ BP/scripts/attributes/registry/scale.ts
@@ -0,0 +1,55 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later
+ * ============================================================================
+ * Commands Plus Plus
+ * Copyright (C) 2024-2026 jeanmajid and contributors
+ * https://github.com/jeanmajid/MCPE-Commands-plus-plus
+ * ============================================================================
+ *
+ * This file is part of Commands Plus Plus.
+ *
+ * Commands Plus Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Commands Plus Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Commands Plus Plus. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { ScoreboardObjective, system } from "@minecraft/server";
+
+import { getAllEntities } from "../../utils/dimension";
+import { AttributeManager, BaseAttribute } from "../attribute";
+
+class ScaleAttribute extends BaseAttribute {
+    public id = "scale";
+    public runId = -1;
+
+    public initialize(): void {
+        this.runId = system.runInterval(() => {
+            this.setValues(this.score);
+        }, 1);
+    }
+
+    public setValues(score: ScoreboardObjective): void {
+        for (const entity of getAllEntities()) {
+            const scale = entity.getComponent("scale")?.value;
+            if (scale === undefined) {
+                return;
+            }
+
+            score.setScore(entity, scale);
+        }
+    }
+
+    public cleanup(): void {
+        system.clearRun(this.runId);
+    }
+}
+
+AttributeManager.registerAttribute(new ScaleAttribute());
