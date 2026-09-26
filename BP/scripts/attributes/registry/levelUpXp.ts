e9e6c6606dbf3c9d7afe9a42e38383f1e7b8a0d0
jeanmajid <jeanhodul@gmail.com>
2026-09-26 15:41:40 +0200

    refactor: rename levelupXp to filename levelUpXp

--- BP/scripts/attributes/registry/levelUpXp.ts
+++ BP/scripts/attributes/registry/levelUpXp.ts
@@ -25,7 +25,7 @@ import { ScoreboardObjective, system, world } from "@minecraft/server";
 
 import { AttributeManager, BaseAttribute } from "../attribute.js";
 
-class LevelupXpAttribute extends BaseAttribute {
+class LevelUpXpAttribute extends BaseAttribute {
     public id = "levelUpXp";
     public runId = -1;
 
@@ -46,4 +46,4 @@ class LevelupXpAttribute extends BaseAttribute {
     }
 }
 
-AttributeManager.register(new LevelupXpAttribute());
+AttributeManager.register(new LevelUpXpAttribute());
